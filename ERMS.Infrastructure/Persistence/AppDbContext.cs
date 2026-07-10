3b17fcaaed7ddc986252e53327c6789514e2b046
onlystappen <34ardasandikci@gmail.com>
2026-07-10 11:07:31 +0300

    Domain,Infrastructure bitti

--- ERMS.Infrastructure/Persistence/AppDbContext.cs
+++ ERMS.Infrastructure/Persistence/AppDbContext.cs
@@ -8,6 +8,28 @@ namespace ERMS.Infrastructure.Persistence
 {
     public class AppDbContext : DbContext
     {
-        public DbSet<>
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+
+        public DbSet<User> Users { get; set; }
+        public DbSet<RequestType> RequestTypes { get; set; }
+        public DbSet<Request> Requests { get; set; }
+
+        public DbSet<RequestAttachment> RequestAttachments { get; set; }
+        public DbSet<RequestComment> RequestComments { get; set; }
+        public DbSet<RequestHistory> RequestHistories { get; set; }
+        public DbSet<Department> Departments { get; set; }
+        public DbSet<Approval> Approvals { get; set; }
+
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<User>()
+                .HasIndex(u => u.Email)
+                .IsUnique();
+
+
+        }
     }
 }

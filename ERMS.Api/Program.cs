5506ac86edce216a97521445bc5dd9e0c346c470
onlystappen <34ardasandikci@gmail.com>
2026-07-10 16:15:15 +0300

    DI Eklendi

--- ERMS.Api/Program.cs
+++ ERMS.Api/Program.cs
@@ -1,3 +1,4 @@
+using ERMS.Infrastructure;
 using ERMS.Infrastructure.Persistence;
 using Microsoft.EntityFrameworkCore;
 
@@ -6,7 +7,7 @@ builder.Services.AddDbContext<AppDbContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 // Add services to the container.
-
+builder.Services.AddInfrastructure();
 builder.Services.AddControllers();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
---
+++ ERMS.Infrastructure/DependencyInjection.cs
@@ -0,0 +1,17 @@
+﻿using ERMS.Infrastructure.Authentication;
+using ERMS.Application.Common.Interfaces;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace ERMS.Infrastructure
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddInfrastructure(this IServiceCollection services)
+        {
+            services.AddScoped<IJwtTokenGenerator, JwtTokenGenerator>();
+            return services;
+        }
+
+
+    }
+}

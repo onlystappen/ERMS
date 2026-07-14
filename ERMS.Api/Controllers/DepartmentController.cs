2c80e2e5c60cac32942e8c1a68e51c9e2f342fb6
onlystappen <34ardasandikci@gmail.com>
2026-07-14 09:51:24 +0300

    Department Service eklendi

---
+++ ERMS.Api/Controllers/DepartmentController.cs
@@ -0,0 +1,27 @@
+﻿using ERMS.Application.DTOs;
+using ERMS.Application.Services;
+using Microsoft.AspNetCore.Mvc;
+
+
+
+
+namespace ERMS.Api.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class DepartmentsController : ControllerBase
+    {
+        private readonly DepartmentService _departmentService;
+
+        public DepartmentsController( DepartmentService departmentService)
+        {
+            _departmentService = departmentService;
+        }
+        [HttpGet]
+        public async Task<ActionResult<List<DepartmentDto>>> GetAll()
+        {
+            var departments = await _departmentService.GetAllDepartmentsAsync();
+            return Ok(departments);
+        }
+    }
+}

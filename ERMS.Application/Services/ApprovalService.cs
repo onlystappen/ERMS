9fe187113a6ecd3f2e07a439c7c246963177d876
onlystappen <34ardasandikci@gmail.com>
2026-07-16 18:29:54 +0300

    güncel

---
+++ ERMS.Application/Services/ApprovalService.cs
@@ -0,0 +1,19 @@
+﻿using ERMS.Application.Common.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ERMS.Application.Services
+{
+    public class ApprovalService
+    {
+        private readonly IApplicationDbContext _context;
+
+        public ApprovalService(IApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+
+    }
+}

465477f59802ae999678a8bfd1733a4e28887f7b
onlystappen <34ardasandikci@gmail.com>
2026-07-16 18:23:30 +0300

    Güncel

--- ERMS.Domain/Entities/Approval.cs
+++ ERMS.Domain/Entities/Approval.cs
@@ -1,21 +1,23 @@
-﻿using Microsoft.Azure.Documents;
-using System;
+﻿using System;
+
 namespace ERMS.Domain.Entities
 {
-
     public class Approval
     {
         public int ApprovalId { get; set; }
         public int RequestId { get; set; }
         public Request Request { get; set; } = null!;
 
-
         public int ApproverId { get; set; }
-        public Microsoft.Azure.Documents.User Approver { get; set; } = null!;
+        
+        public User Approver { get; set; } = null!;
 
+        
         public string Decision { get; set; } = string.Empty;
 
         public string? Comment { get; set; }
-        public DateTime DecidedAt { get; set; }
+
+        
+        public DateTime? DecidedAt { get; set; }
     }
 }

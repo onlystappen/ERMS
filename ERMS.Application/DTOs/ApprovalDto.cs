8fcfcba9ae39126f55d803672f2c7b41d883c7e5
onlystappen <34ardasandikci@gmail.com>
2026-07-16 18:25:31 +0300

    güncel

---
+++ ERMS.Application/DTOs/ApprovalDto.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace ERMS.Application.DTOs
+{
+    public class ApprovalDto
+    {
+        public int ApprovalId { get; set; }
+        public int RequestId { get; set; }
+        public int ApproverId { get; set; }
+        public string Decision { get; set; } = string.Empty;
+        public string? Comment { get; set; }
+        public DateTime? DecidedAt { get; set; }
+
+        
+        public string? ApproverName { get; set; }
+    }
+}

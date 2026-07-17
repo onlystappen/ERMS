99af71fcac9f68b7665322f504df8759da033726
onlystappen <34ardasandikci@gmail.com>
2026-07-17 09:21:52 +0300

    güncel

---
+++ ERMS.Api/Controllers/ApprovalController.cs
@@ -0,0 +1,19 @@
+﻿using ERMS.Application.Services;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ERMS.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ApprovalController : ControllerBase
+    {
+        private readonly ApprovalService _approvalService;
+        public ApprovalController(ApprovalService approvalService)
+        {
+            _approvalService = approvalService;
+        }
+
+
+    }
+}

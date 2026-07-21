5320ce4834b8cc35434059535e990867bede5dc1
onlystappen <34ardasandikci@gmail.com>
2026-07-21 11:22:32 +0300

    güncel

---
+++ ERMS.Domain/Entities/AuditLog.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ERMS.Domain.Entities
+{
+    public class AuditLog
+    {
+        public int AuditLogId { get; set; }
+        public int? UserId { get; set; }
+        public string Action { get; set; } = string.Empty;
+        public string EntityName { get; set; } = string.Empty;
+        public int EntityId { get; set; }
+        public string Details { get; set; } = string.Empty;
+        public DateTime Timestamp { get; set; } = DateTime.UtcNow;
+    }
+}
